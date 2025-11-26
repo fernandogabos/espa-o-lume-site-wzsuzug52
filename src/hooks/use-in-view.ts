b4892d5e4c2d0e75308b26824f64bb9dc302beaa
Skip <skip@usecurling.com>
2025-11-26 16:12:31 +0000

    Implementa layout global e home page

--- src/hooks/use-in-view.ts
+++ src/hooks/use-in-view.ts
@@ -15,13 +15,15 @@ export function useInView(options?: IntersectionObserverInit) {
       }
     }, options)
 
-    if (ref.current) {
-      observer.observe(ref.current)
+    const currentRef = ref.current
+
+    if (currentRef) {
+      observer.observe(currentRef)
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current)
+      if (currentRef) {
+        observer.unobserve(currentRef)
       }
     }
   }, [options])
